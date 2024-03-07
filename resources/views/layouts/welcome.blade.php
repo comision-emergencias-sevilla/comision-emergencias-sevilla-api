98c761ce3c712d4c6ed458cd3b35ed73e1b06f8d
Jorge Juan Rodríguez Manzano <34060303+jjrmanzano@users.noreply.github.com>
2024-03-07 20:04:04 +0100

    Add mobile menu for landing page

--- resources/views/layouts/welcome.blade.php
+++ resources/views/layouts/welcome.blade.php
@@ -17,15 +17,15 @@
       <div class="bg-white">
         <!-- Header -->
         <header class="absolute inset-x-0 top-0 z-50">
-          <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+          <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global" id="nav">
             <div class="flex lg:flex-1">
-              <a href="https://trabajosocialsevilla.es" class="-m-1.5 p-1.5">
+              <a href="/" class="-m-1.5 p-1.5">
                 <span class="sr-only">Comisión de Emergencias - Sevilla</span>
-                <img class="h-16 w-auto grayscale" src="assets/images/logo_grid.webp" alt="Colegio Oficial de Trabajo Social de Sevilla">
+                <img class="h-16 w-auto grayscale" src="assets/images/logo_grid.webp" alt="Logo del Colegio Oficial de Trabajo Social de Sevilla">
               </a>
             </div>
             <div class="flex lg:hidden">
-              <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+              <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" onclick="mobileMenu.show()">
                 <span class="sr-only">Abrir menú principal</span>
                 <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
@@ -39,9 +39,41 @@
               <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Mapa de Sevilla (en construcción)</a>
             </div>
             <div class="hidden lg:flex lg:flex-1 lg:justify-end">
-              <a href="/login" class="text-sm font-semibold leading-6 text-gray-900">Acceder <span aria-hidden="true">&rarr;</span></a>
+              <a href="/login" class="text-sm font-semibold leading-6 text-gray-900">Acceso <span aria-hidden="true">&rarr;</span></a>
             </div>
           </nav>
+
+          <!-- Mobile menu -->
+          <div class="hidden lg:hidden" role="dialog" aria-modal="true" id="mobileMenu">
+            <div class="fixed inset-0 z-50"></div>
+            <div class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+              <div class="flex items-center justify-between">
+                <a href="/" class="-m-1.5 p-1.5">
+                  <span class="sr-only">Comisión de Emergencias - Sevilla</span>
+                  <img class="h-16 w-auto grayscale" src="assets/images/logo_grid.webp" alt="Logo del Colegio Oficial de Trabajo Social de Sevilla">
+                </a>
+                <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" onclick="mobileMenu.hide()">
+                  <span class="sr-only">Cerrar menu</span>
+                  <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                  </svg>
+                </button>
+              </div>
+              <div class="mt-6 flow-root">
+                <div class="-my-6 divide-y divide-gray-500/10">
+                  <div class="space-y-2 py-6">
+                    <a href="/assets/docs/Guía de Recursos de Emergencias 2020 - COTSS.pdf" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50" download>Guía de Recursos</a>
+                    <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Residencias y pisos tutelados (en construcción)</a>
+                    <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Primeros auxilios psicológicos (en construcción)</a>
+                    <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Mapa de Sevilla (en construcción)</a>
+                  </div>
+                  <div class="py-6">
+                    <a href="/login" class="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Acceso <span aria-hidden="true">&rarr;</span></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </header>
 
         <main class="isolate">
@@ -68,6 +100,24 @@
           </div>
         </main>
       </div>
+      <script type="text/javascript">
+        
+        class MobileMenu {
+
+          show() {
+              document.getElementById('nav').classList.add('hidden');
+              document.getElementById('mobileMenu').classList.remove('hidden');
+          }
+
+          hide() {
+            document.getElementById('nav').classList.remove('hidden');
+            document.getElementById('mobileMenu').classList.add('hidden');
+          }
+
+        }
+
+        const mobileMenu = new MobileMenu();
+      </script>
     </body>
 </html>
 
